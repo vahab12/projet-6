fa0746fe4b23d1de503e6d785f027336937e69e9
vahab12 <vahabtanha@yahoo.com>
2021-11-17 10:20:05 +0100

    creation de CRUD pour sauce

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-//app.use('/api/sauces', sauceRoutes);
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
---
+++ backend/routes/sauce.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const saucesCtrl = require('../controllers/sauce');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+
+router.post('/', auth, multer, saucesCtrl.createSauce);
+router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+router.get('/', auth, saucesCtrl.getAllSauce);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
+
+
+module.exports = router;
