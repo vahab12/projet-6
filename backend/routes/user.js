e9bb740743ac867bbb0347e2c8bcab6dfaa1eadd
vahab12 <vahabtanha@yahoo.com>
2021-11-22 14:52:58 +0100

    Ajaut de checker d'email

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
-const passwordValidator = require("../middleware/password-validator")
+const passwordValidator = require('../middleware/password-validator')
+const emailChecker = require('../middleware/email-checker')
 
-router.post('/signup', passwordValidator, userCtrl.signup);
+router.post('/signup', passwordValidator, emailChecker, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
